39444862e50b7e3948055683b3a6dc0515c7098c
Alok Kumar Singh <alok18121991@gmail.com>
2021-05-09 23:30:40 +0530

    highlighting of nav added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
 import Welcome from './components/welcomePage/welcome';
@@ -7,11 +6,12 @@ import Projects from './components/projectView/projectView';
 import Skills from './components/skillsView/skillsView';
 import Contact from './components/contactView/contactView';
 import Footer from './components/footer/footer';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App p-2 bg-dark" >
+      <BrowserRouter>
       <Header />
         <Switch>
           <Route path="/home">
@@ -30,28 +30,8 @@ function App() {
             <Contact />
           </Route>
         </Switch>
-
-      {/* <Welcome/> */}
-       {/* <Experience/>
-      <Projects />
-      <Skills />
-      <Contact /> */}
       <Footer />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
+      </BrowserRouter>
     </div>
 
   );
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeKey: "/home"
+        };
+    }
+
+    updateActiveKey = key => {
+        this.setState({ activeKey: key });
+    }
+
     render() {
+
         return (
-            
-                <Navbar bg="dark" variant="dark" className="fixed-top px-5"> 
-                    <Navbar.Brand href="/">Alok Kumar Singh</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse>
-                        <Nav className="justify-content-end" style={{ width: "100%" }}>
-                            <Nav.Item>
-                                <Nav.Link href="/home">Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/experience">Experience</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/projects">Projects</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/skills">Skills</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="/contact">Contact</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+            <Navbar bg="dark" variant="dark" className="fixed-top px-5">
+                <Navbar.Brand href="/">Alok Kumar Singh</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    {console.log("herre.. ", this.state.activeKey)}
+                    <Nav className="justify-content-end" style={{ width: "100%" }}
+                        activeKey={`${this.state.activeKey}`}
+                        onSelect={(selectedKey) => this.updateActiveKey(selectedKey)
+                        }
+                    >
+                        <Nav.Item>
+                            <Nav.Link as={Link} eventKey={"/home"} to="/home">Home</Nav.Link> 
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} eventKey={"/experience"} to="/experience">Experience</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} eventKey={"/projects"} to="/projects">Projects</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} eventKey={"/skills"} to="/skills">Skills</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link as={Link} eventKey={"/contact"} to="/contact">Contact</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
